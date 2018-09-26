9c365e70bd7858a6a32ab76592f327871fbb85df
ebrault <emmet.brault@gmail.com>
2018-09-26 14:46:00 -0400

    Made changes
    Added JS app code to JS branch

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,16 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const userEvents = require('user/events.js')
+const gameEvents = require('game/events.js')
+
 $(() => {
-  // your JS code goes here
+  $('#sign-up-form').on('submit', () => {})
+  $('#sign-in-form').on('submit', () => {})
+  $('#sign-out-button').on('click', () => {})
+  $('#change-password-form').on('submit', () => {})
+  $('#new-game-button').on('click', () => {})
+  $('#get-all-games-button').on('click', () => {})
+  $('#get-one-game-form').on('submit', () => {})
+  $('.update-game').on('click', () => {})
 })
