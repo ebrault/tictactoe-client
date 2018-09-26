2a4479298440348b6268f0616fffd5911442748b
ebrault <emmet.brault@gmail.com>
2018-09-26 14:44:21 -0400

    Made Changes
    Put UI function into exception handling

--- assets/scripts/user/events.js
+++ assets/scripts/user/events.js
@@ -8,30 +8,30 @@ const onSignUp = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
   api.signUp(data)
-    .then(() => {})
-    .catch(() => {})
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
 }
 
 const onSignIn = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
   api.signIn(data)
-    .then(() => {})
-    .catch(() => {})
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
 }
 
 const onChangePassword = function (event) {
   event.preventDefault()
   const data = getFormFields(event.target)
   api.changePassword(data)
-    .then(() => {})
-    .catch(() => {})
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
 }
 
 const onSignOut = function (event) {
   api.signOut()
-    .then(() => {})
-    .catch(() => {})
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
 }
 
 module.exports = {
