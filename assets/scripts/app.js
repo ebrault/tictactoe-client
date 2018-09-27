42f27b46a4f429248ff8694a2161d22a20fc1a63
ebrault <emmet.brault@gmail.com>
2018-09-27 15:11:58 -0400

    Made Changes
    Moved game logic to its own branch

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,4 +10,71 @@ const userEvents = require('user/events.js')
 const gameEvents = require('game/events.js')
 */
 
-$(() => {})
+$(() => {
+  let turn = 'x'
+  const gameBoard = ['', '', '', '', '', '', '', '', '']
+  let gameOver = false
+  console.log('It is Xs Turn!')
+  const click = function (square) {
+    $('#square' + String(square)).html = turn
+    if (gameOver === true) {
+    } else if (gameBoard[square] === '') {
+      gameBoard[square] = turn
+      const statusOfWinnerCheck = checkForWinner()
+      if (statusOfWinnerCheck) {
+        if (statusOfWinnerCheck === 'draw') {
+        } else {
+        }
+        console.log(turn + 'has won the game!')
+        gameOver = true
+      }
+    }
+    if (turn === 'x') {
+      turn = 'o'
+      console.log(`It is ${turn}'s turn'`)
+    } else if (turn === 'o') {
+      turn = 'x'
+      console.log(`It is ${turn}'s turn`)
+    }
+  }
+
+  const checkForWinner = function () {
+    if (gameBoard[0] !== '') {
+      if (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2]) {
+        return turn
+      } else if (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6]) {
+        return turn
+      }
+    }
+    if (gameBoard[4] !== '') {
+      if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8]) {
+        return turn
+      } else if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6]) {
+        return turn
+      } else if (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[7]) {
+        return turn
+      } else if (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5]) {
+        return turn
+      }
+    }
+    if (gameBoard[8] !== '') {
+      if (gameBoard[8] === gameBoard[5] && gameBoard[5] === gameBoard[2]) {
+        return turn
+      } else if (gameBoard[8] === gameBoard[7] && gameBoard[7] === gameBoard[6]) {
+        return turn
+      }
+    }
+    let isADraw = false
+    for (let i = 0; i <= gameBoard.length; i++) {
+      if (gameBoard[i] === '') {
+        return
+      } else if (i === 8) {
+        isADraw = true
+      }
+    } if (isADraw) {
+      return console.log('Draw')
+    } else {
+      return false
+    }
+  }
+})
