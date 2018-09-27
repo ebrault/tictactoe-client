394aa61fd24e5a11095737d4a4492c6151114c1a
ebrault <emmet.brault@gmail.com>
2018-09-27 14:04:54 -0400

    Commented out game logic in app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,8 +6,8 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-const userEvents = require('user/events.js')
-const gameEvents = require('game/events.js')
+const userEvents = require('./user/events.js')
+const gameEvents = require('./game/events.js')
 
 $(() => {
   /*
